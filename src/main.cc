4dd27be380c2bc3289e39b59cb14e343317dbb7f
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2015-05-07 21:58:59 +0800

    reduce obstacle in main

--- src/main.cc
+++ src/main.cc
@@ -1,19 +1,18 @@
 #include "moge/system.hh"
 #include "moge/window.hh"
-#include "moge/meta/bind.hh"
 #include "moge/meta/optional.hh"
+using namespace moge;
 
 int main()
 {
-	moge::window win;
-	win.vsync();
-	win.events->close = [] { moge::system::quit(); };
+	window win1{"hello world"};
+	win1.vsync();
+	win1.events->close = [] { system::quit(); };
 
-	moge::optional<moge::window> win2;
-	win2 = moge::window{"window 2", {320, 240}};
-	win2->vsync();
+	optional<window> win2;
+	win2 = window{"window 2", {320, 240}};
 	win2->events->close = [&] { win2 = {}; };
 
-	moge::system::run();
+	system::run();
 }
 
