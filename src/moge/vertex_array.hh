9a2c1c3ce0a2f4215cc8030184830e1e09a4baeb
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2015-05-11 18:24:31 +0800

    + vertex array

---
+++ src/moge/vertex_array.hh
@@ -0,0 +1,60 @@
+#pragma once
+#include "gl.hh"
+#include "meta/resource.hh"
+#include "meta/resource-traits.hh"
+
+namespace moge
+{
+	namespace vertex_array_detail
+	{
+		struct vertex_array;
+	}
+
+	using vertex_array_detail::vertex_array;
+
+
+	template <>
+	struct resource_traits<vertex_array> : make_resource_traits<GLuint>
+	{
+		static auto allocate() { value_type x; glGenVertexArrays(1, &x); return x; }
+		static void deallocate(value_type const& x) { glDeleteVertexArrays(1, &x); }
+		static void bind(value_type const& x) { glBindVertexArray(x); }
+	};
+
+
+	namespace vertex_array_detail
+	{
+		struct vertex_array : resource<vertex_array>
+		{
+			auto& draw_point(GLsizei count, int first=0)
+			{
+				return draw<GL_POINTS>(count, first);
+			}
+
+			auto& draw_line_loop(GLsizei count, int first=0)
+			{
+				return draw<GL_LINE_LOOP>(count, first);
+			}
+
+			auto& draw_triangle(GLsizei count, int first=0)
+			{
+				return draw<GL_TRIANGLES>(count, first);
+			}
+
+			auto& draw_triangle_strip(GLsizei count, int first=0)
+			{
+				return draw<GL_TRIANGLE_STRIP>(count, first);
+			}
+
+		private:
+			template <GLenum MODE>
+			vertex_array& draw(GLsizei count, int first=0)
+			{
+				auto _ = bind();
+				glDrawArrays(MODE, first, count);
+				return *this;
+			}
+		};
+	}
+}
+
