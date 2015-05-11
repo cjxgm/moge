a4564465448bb859c2ccaaa0ea5f084b813a9c9e
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2015-05-11 19:05:50 +0800

    test

--- src/main.cc
+++ src/main.cc
@@ -7,6 +7,8 @@
 #include "moge/shader.hh"
 #include "moge/program.hh"
 #include "moge/utils.hh"
+#include "moge/vertex_array.hh"
+#include "moge/buffer.hh"
 using namespace moge;
 
 namespace
@@ -25,12 +27,27 @@ namespace
 int main()
 {
 	window win1;
+	win1.bind();
+
+	array_buffer buf;
+	buf.bind();
+	float points[] = {
+		-0.5, -0.5,
+		+0.3, +0.4,
+	};
+	buf.data(buffer_usage::static_draw, points, sizeof(points));
+
+	vertex_array arr;
+	arr.bind();
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, 0);
+
+	auto p1 = load_program(win1);
 
 	optional<window> win2;
 	win2 = window{"window 2", {320, 240}};
 	win2->events->close = [&] { win2 = {}; };
 
-	auto p1 = load_program(win1);
 	auto p2 = load_program(*win2);
 
 	auto& sys = system::instance();
@@ -40,6 +57,7 @@ int main()
 			auto p_ = p1.bind();
 			clear_color({1, 1, 1, 1});
 			clear(clear_target::color_buffer);
+			arr.draw_point(2);
 			win1.update();
 		}
 
